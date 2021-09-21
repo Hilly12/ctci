0a6281216300641b5e13615fcbe1535f2551d821
Aahil Mehta <aahil@aahil-arch.localdomain>
2021-09-21 14:37:19 +0100

    ch1

---
+++ 1-arrays-and-strings.py
@@ -0,0 +1,111 @@
+from typing import Sequence
+
+from collections import defaultdict
+
+
+def is_unique(s: str) -> bool:
+    s = sorted(s)
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            return False
+
+    return True
+
+
+def check_perm(s1: str, s2: str) -> bool:
+    table = set(s1)
+
+    for c in s2:
+        if c not in table:
+            return False
+
+    return True
+
+
+def urlify(s: str) -> str:
+    s = list(s)
+    queue = []
+    for i, c in enumerate(s):
+        if c == " ":
+            queue.append('%')
+            queue.append('2')
+            queue.append('0')
+        else:
+            queue.append(c)
+        
+        s[i] = queue.pop(0)
+
+    while len(queue) > 0:
+        s.append(queue.pop(0))
+
+    return "".join(s)
+
+
+def palindrome_perm(s: str) -> bool:
+    table = defaultdict(int)
+    for c in s.lower():
+        table[c] += 1
+
+    odds = 0
+    for c, freq in table.items():
+        if freq % 2 == 1:
+            odds += 1
+
+    return odds <= 2
+
+
+def one_away(s1: str, s2: str) -> bool:
+    if s1 == s2:
+        return True
+
+    if abs(len(s1) - len(s2)) >= 2:
+        return False
+
+    if len(s1) > len(s2):
+        temp = s1
+        s1 = s2
+        s2 = temp
+
+    for i in range(len(s1) + 1):
+        rem1 = (s1[:i] + s1[i+1:])
+        rem2 = (s2[:i] + s2[i+1:])
+        if s1 == rem2 or rem1 == rem2:
+            return True
+    
+    return False
+
+
+def string_compression(s: str) -> str:
+    if len(s) == 0:
+        return s
+
+    count = 0
+    curr = s[0]
+    rle = []
+    for i in range(len(s)):
+        if s[i] == curr:
+            count += 1
+        else:
+            rle.append((curr, count))
+            count = 1
+            curr = s[i]
+    
+    rle.append((curr, count))
+    
+    cmp = "".join([str(c) + str(cnt) for c, cnt in rle])
+
+    if len(cmp) < len(s):
+        return cmp
+    
+    return s
+
+
+def rotate_matrix(m: Sequence[Sequence[int]]) -> Sequence[Sequence[int]]:
+    l = len(m)
+    for y in range(l):
+        for x in range(l):
+            m[x][l - 1 - y] = m[y][x]
+    
+    return m
+
+print(rotate_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
